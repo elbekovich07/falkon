a3739a97b4867cfc65138c03cb7f9ef2e08d92df
normuminov <olmosnormuminov02@gmail.com>
2025-04-17 13:51:44 +0500

    'd'

--- root/settings.py
+++ root/settings.py
@@ -94,11 +94,11 @@ WSGI_APPLICATION = 'root.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DATABASE_NAME'),
-        'USER': os.getenv('DATABASE_USER'),
-        'PASSWORD': os.getenv('DATABASE_PASSWORD'),
-        'HOST': os.getenv('DATABASE_HOST'),
-        'PORT': os.getenv('DATABASE_PORT'),
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'VxxkokkNgLzEolMivIXctJraNNIBZFcr',
+        'HOST': 'shortline.proxy.rlwy.net',
+        'PORT': 42192,
     }
 }
 
